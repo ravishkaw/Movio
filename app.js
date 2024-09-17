fa15bded58b794e125ba898bd996782640b00124
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-09-17 22:55:47 +0530

    restructure the project using middlewares

--- app.js
+++ app.js
@@ -1,74 +1,13 @@
-const { v4: uuidv4 } = require("uuid");
-const { validateGenre } = require("./validations");
+const genres = require("./routes/genres");
+const home = require("./routes/home");
 
 const express = require("express");
 const app = express();
 
-const genres = [
-  { id: uuidv4(), name: "action" },
-  { id: uuidv4(), name: "animation" },
-];
-
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello welcome to Movio");
-});
-
-app.get("/api/movies/genres", (req, res) => {
-  res.send(genres);
-});
-
-app.post("/api/movies/genres", (req, res) => {
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-  const genre = {
-    id: uuidv4(),
-    name: req.body.name,
-  };
-  genres.push(genre);
-  res.status(201).send(genre);
-});
-
-app.get("/api/movies/genres/:id", (req, res) => {
-  const genre = genres.find((gen) => gen.id === req.params.id);
-
-  if (!genre) {
-    return res.status(404).send("Genre not found");
-  }
-
-  res.send(genre);
-});
-
-app.put("/api/movies/genres/:id", (req, res) => {
-  const genre = genres.find((gen) => gen.id === req.params.id);
-
-  if (!genre) {
-    return res.status(404).send("Genre not found");
-  }
-
-  const { error } = validateGenre(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  genre.name = req.body.name;
-  res.send(genre);
-});
-
-app.delete("/api/movies/genres/:id", (req, res) => {
-  const genre = genres.find((gen) => gen.id === req.params.id);
-
-  if (!genre) {
-    return res.status(404).send("Genre not found");
-  }
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-  res.send({ message: "Genre deleted successfully", deletedGenre: genre }); 
-});
+app.use("/", home);
+app.use("/api/genres", genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
---
+++ routes/genres.js
@@ -0,0 +1,67 @@
+const { v4: uuidv4 } = require("uuid");
+const { validateGenre } = require("./validations");
+
+const express = require("express");
+const router = express.Router();
+
+const genres = [
+  { id: uuidv4(), name: "action" },
+  { id: uuidv4(), name: "animation" },
+];
+
+router.get("/", (req, res) => {
+  res.send(genres);
+});
+
+router.post("/", (req, res) => {
+  const { error } = validateGenre(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+  const genre = {
+    id: uuidv4(),
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.status(201).send(genre);
+});
+
+router.get("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+
+  res.send(genre);
+});
+
+router.put("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+
+  const { error } = validateGenre(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  genre.name = req.body.name;
+  res.send(genre);
+});
+
+router.delete("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === req.params.id);
+
+  if (!genre) {
+    return res.status(404).send("Genre not found");
+  }
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+  res.send({ message: "Genre deleted successfully", deletedGenre: genre });
+});
+
+module.exports = router;
