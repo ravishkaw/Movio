181099f6e67f9f9e0a2db0da890731fcfd3cf3ce
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-09-17 18:57:25 +0530

    add genre get and create

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
+const { v4: uuidv4 } = require("uuid");
 const express = require("express");
 const app = express();
+
+const genres = [
+  { id: uuidv4(), name: "action" },
+  { id: uuidv4(), name: "animation" },
+];
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello welcome to Movio");
+});
+
+app.get("/api/movies/genres", (req, res) => {
+  res.send(genres);
+});
+
+app.post("/api/movies/genres", (req, res) => {
+  const genre = {
+    id: uuidv4(),
+    name: req.body.name,
+  };
+  genres.push(genre);
+  res.send(genres);
 });
 
 const port = process.env.PORT || 3000;
